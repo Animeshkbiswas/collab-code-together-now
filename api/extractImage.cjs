5558dad2d4c5165680b5264fcb9031828bc692f9
Animeshkbiswas <animeshbiswas15122004@gmail.com>
2025-06-24 20:36:03 +0530

    Implement playlist manager close button and bugfix

--- api/extractImage.cjs
+++ api/extractImage.cjs
@@ -4,6 +4,12 @@ const { createWorker } = require('tesseract.js');
 
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') {
+    res.status(204).end();
+    return;
+  }
   if (req.method !== 'POST') {
     res.status(405).json({ error: 'Method not allowed' });
     return;
